783e8e4e9129a9ba664c86ffe9da4fcbab5ff9d9
Wintxer <47366828+Wintxer@users.noreply.github.com>
2022-12-22 14:49:19 +0100

    fix

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
     $data = json_decode($json);
     file_put_contents( 'data.log', print_r( $data, true ), FILE_APPEND );
 
+    $newdata = file_get_contents('data.log');
     var_dump($data);
+    var_dump($newdata);
     die();
 ?>
 <!DOCTYPE html>
