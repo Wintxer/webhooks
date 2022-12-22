b683ad067481a5919a6fe62951852213a10d058c
Wintxer <47366828+Wintxer@users.noreply.github.com>
2022-12-22 14:14:45 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+    file_put_contents( 'verify.log', print_r( $_GET, true ), FILE_APPEND );
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Instagram Graph API Webhooks</h1>
+</body>
+</html>
